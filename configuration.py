192456b1c29536561d1af9871566dfce01aaa0ff
Ishaan Kohli <130181481+Ginga1402@users.noreply.github.com>
2025-11-04 23:39:57 +0530

    configuration.py
    Added environment variable setup and OpenAI model configuration.

--- configuration.py
+++ configuration.py
@@ -1,2 +1,21 @@
+from strands.models.openai import OpenAIModel
+import os
+
+
+## Configurations and Keys
+DB_FILE = 'financial_data.db'
+
+TAVILY_API_KEY = "YOUR-TAVILY-KEY"
+
+OPENAI_KEY = "YOUR-OPENAI-KEY"
+
+os.environ['OPENAI_API_KEY'] = OPENAI_KEY
+gpt_model = OpenAIModel(model_id = "gpt-3.5-turbo", 
+                        temperature = 0.0,
+                        params={
+                        "temperature": 0.0 
+                        })
+
+
 
 
